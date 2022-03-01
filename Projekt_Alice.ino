34fb4de2069c782b69be084ba9b3b95c89ed0a1d
Zachorowski <65033014+Zachorowski@users.noreply.github.com>
2022-03-01 13:25:03 +0100

    Ujednolicenie kodu
    Poprawiona obsługa przycisku, dodana obsługa większej ilości komend, ujednolicenie językowe kodu

--- Projekt_Alice.ino
+++ Projekt_Alice.ino
@@ -1,59 +1,95 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
+/*Alice project controler 
+ * Stanisław Zachorowski (zstaszek@gmail.com)
+ * Katarzyna Pióro
+ * 2022
+ */
+//libraries
+#include <OneWire.h> // one wire protocol
+#include <DallasTemperature.h> // DS18B20 thermometer handling
 
-//deklaracja pinów
-#define diodaPWM 4
-#define przycisk 3
-#define stycznik 2
+//pin declaration
+#define LEDindicator 4
+#define PWM 5
+#define button 3
+#define contactor 2
 
-//deklaracja zmiennych 
-float temp = 0; //temperatura
-int order = 0; //rozkaz 
-volatile int state = HIGH; // stan systemu
+//variable declaration 
+float temp = 0; //themperature
+int order = 0; //serial orders 
+volatile int system_state = LOW; // system state
+volatile int fan_state = LOW;
 
-//deklaracja termometru
+//thermometer declaration
 OneWire OneWire(12);
-DallasTemperature czujnik(&OneWire);
+DallasTemperature thermometer(&OneWire);
 
-//funkcja obsługująca przerwanie od fizycznego przycisku 
-void funckjaObslugiPrzerwania() {
-  if(state == HIGH)
+//physical turning ON/OFF
+void changeOfStates() {
+  if(system_state == HIGH)
   {
-    state = LOW;
-    digitalWrite(stycznik, LOW);
-    Serial.println("wyłączono urządzenie");
-    delay(200);
+    system_state = LOW;
+    digitalWrite(LEDindicator,LOW);
+    digitalWrite(contactor, HIGH);
+    Serial.println("Power OFF button");
+    delay(500);
   }
   else
   {
-    state = HIGH;
-    digitalWrite(stycznik, HIGH);
-    Serial.println("włączono urządzenie");
-    delay(200);
-    
-  }
-  
+    system_state = HIGH;
+    digitalWrite(LEDindicator,HIGH);
+    digitalWrite(contactor, LOW);
+    Serial.println("Power ON button");
+    delay(500);  
+  }  
 }
 
-//funkcja obsługi komunikacji przez port szeregowy 
+//serial connection handling 
 void checkSerial() 
 {
   if (Serial.available()>0) 
   { 
     order = Serial.read();
-    if(order == '1')
+    if(order == '0')
     {
-      state = HIGH;
-      digitalWrite(stycznik, HIGH);
-      Serial.println("włączono urządzenie");
+      system_state = LOW;
+      digitalWrite(LEDindicator,LOW);
+      digitalWrite(contactor,HIGH);
+      Serial.println("Power OFF");
+      delay (100);
+    }
+    else if(order == '1')
+    {
+      system_state = HIGH;
+      digitalWrite(LEDindicator,HIGH);
+      digitalWrite(contactor,LOW);
+      Serial.println("Power ON");
       delay(100);
     }
-    else if(order == '0')
+    else if(order == '2')
     {
-      state = LOW;
-      digitalWrite(stycznik, LOW);
-      Serial.println("wyłączono urządzenie");
-      delay (100);
+      fan_state = HIGH;
+      Serial.println("Fans automatic controll");     
+    }
+    else if(order == '3')
+    {
+      fan_state = LOW;
+      analogWrite(PWM, 0);
+      Serial.println("Fans OFF");
+    }
+    else if(order == '4')
+    {
+      fan_state = LOW;
+      analogWrite(PWM, 250);
+      Serial.println("Fans full power");
+    }
+    else if(order == '5')
+    {
+      Serial.println("0 - Power OFF");
+      Serial.println("1 - Power ON");
+      Serial.println("2 - Fans automatic controll");
+      Serial.println("3 - Fans OFF");
+      Serial.println("4 - Fans full power");
+      Serial.println("5 - HELP");
     }
     else
     {
@@ -61,46 +97,86 @@ void checkSerial()
     }
   }
 }
-
-
+//fan power controler
+/*void fanControl(float temp)
+{
+  if (fan_state == HIGH)
+  {
+    if (temp > 35)
+     {
+         analogWrite(PWM, 250);
+     }
+     else if (temp > 30)
+     {
+         analogWrite(PWM, 200);
+     }
+     else if (temp > 25)
+     {
+         analogWrite(PWM, 150);
+     }
+     else if (temp > 20)
+     {
+        analogWrite(PWM, 100);
+     }
+     else if (temp > 0)
+     {
+        analogWrite(PWM, 50);
+     }
+     else
+     {
+        analogWrite(PWM, 200);
+     }
+  }
+}  
+*/
 void setup() {
-  attachInterrupt(digitalPinToInterrupt(przycisk), funckjaObslugiPrzerwania, RISING);
-  pinMode(stycznik, OUTPUT); //stycznik
-  pinMode(diodaPWM, OUTPUT); //PWM
-  Serial.begin(9600);
-  czujnik.begin();
+  pinMode(contactor, OUTPUT); //power contactor pin
+  pinMode(PWM, OUTPUT); //PWM controll pin
+  pinMode(button, INPUT_PULLUP); //button pin
+  pinMode(LEDindicator, OUTPUT); //LED pin
+  
+  Serial.begin(9600); //serial conection start
+  thermometer.begin(); //sensor reading start
+  
+  digitalWrite(LEDindicator,HIGH);
 }
 
-void loop() {
-
-  //odczyt temperatury 
-  czujnik.requestTemperatures();
-  temp = czujnik.getTempCByIndex(0);
-
-  //obsługa wiatraków
-         if (temp > 35)
-         {
-             analogWrite(diodaPWM, 250);
-         }
-         else if (temp > 30)
-         {
-             analogWrite(diodaPWM, 200);
-         }
-         else if (temp > 25)
-         {
-             analogWrite(diodaPWM, 150);
-         }
-         else if (temp > 20)
-         {
-            analogWrite(diodaPWM, 100);
-         }
-         else if (temp > 0)
-         {
-            analogWrite(diodaPWM, 50);
-         }
-         else
-         {
-            analogWrite(diodaPWM, 200);
-         }
+void loop() 
+{
+  //themperature read
+  thermometer.requestTemperatures();
+  temp = thermometer.getTempCByIndex(0);
+         
+ if (fan_state == HIGH)
+  {
+    if (temp > 35)
+     {
+         analogWrite(PWM, 250);
+     }
+     else if (temp > 30)
+     {
+         analogWrite(PWM, 200);
+     }
+     else if (temp > 25)
+     {
+         analogWrite(PWM, 150);
+     }
+     else if (temp > 20)
+     {
+        analogWrite(PWM, 100);
+     }
+     else if (temp > 0)
+     {
+        analogWrite(PWM, 50);
+     }
+     else
+     {
+        analogWrite(PWM, 200);
+     }
+  }
+ if(digitalRead(button) == LOW) 
+ {
+   changeOfStates();
+ }
   checkSerial();
 }
